810a2d0de8d40525ea5787a3b5128c700d5acc21
o-data <67723461+o-data@users.noreply.github.com>
2023-03-22 17:22:43 +1100

    Create Lead.php
    https://docs.espocrm.com/development/duplicate-check/

---
+++ Duplicate Check for RealEstate/custom/Espo/Custom/Classes/DuplicateWhereBuilders/Lead.php
@@ -0,0 +1,44 @@
+<?php
+namespace Espo\Custom\Classes\DuplicateWhereBuilders;
+
+use Espo\Core\Duplicate\WhereBuilder;
+
+use Espo\ORM\Query\Part\Condition as Cond;
+use Espo\ORM\Query\Part\WhereItem;
+use Espo\ORM\Query\Part\Where\OrGroup;
+use Espo\ORM\Entity;
+
+class Lead implements WhereBuilder
+{
+    public function build(Entity $entity): ?WhereItem
+    {
+        $orBuilder = OrGroup::createBuilder();
+
+        $toCheck = false;
+
+        if ($entity->get('addressStreet') || $entity->get('addressCity')) {
+            $orBuilder->add(
+                Cond::and(
+                    Cond::equal(
+                        Cond::column('addressStreet'),
+                        $entity->get('addressStreet')
+                    ),
+                    Cond::equal(
+                        Cond::column('addressCity'),
+                        $entity->get('addressCity')
+                    )
+                )
+            );
+
+            $toCheck = true;
+        }
+
+        // Here you can add more conditions.
+
+        if (!$toCheck) {
+            return null;
+        }
+
+        return $orBuilder->build();
+    }
+}
